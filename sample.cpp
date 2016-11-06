3a70bc18a30e1706b467c7257e63d6387884b55b
root <3383789730@qq.com>
2016-11-06 21:12:46 +0800

    2016-10-6-v3

--- sample.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello xjy!" << std::endl;
-std::cout << "Hello xj1y!" << std::endl;
-std::cout << "Hello xj1y!" << std::endl;
-    return 0;
-}
